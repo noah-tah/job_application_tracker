e87f88297222cc79d2d9de35afe8de0ba278d540
Noah Tah <noahtah21@gmail.com>
2026-01-06 10:24:34 -0600

    basic mobile ui complete

--- src/ApplicationList.tsx
+++ src/ApplicationList.tsx
@@ -42,27 +42,54 @@ export default function ApplicationList() {
 
     return (
         <div>
-            <div>
-                <button>Back</button>
-                <h1>Application List</h1>
-                <button>New</button>
+            <div className="flex justify-between items-center p-2 bg-gray-300">
+                <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Back</button>
+                <h1 className="text-2xl font-bold">Application List</h1>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded-md">New</button>
             </div>
-            <div className="application-list-container">
+            <div className="flex flex-col">
                 {
                 applications.length === 0 
                 ? (
                     <p>You haven't added any applications yet, add one below!</p>
                 ) : (
-                    <ul className="application-list">
+                    <ul className="flex flex-col p-2 gap-2">
                         {applications.map((application) => (
-                            <li key={application.id} className="application-item">
-                                <h1>{application.company}</h1>
-                                <h2>{application.role}</h2>
-                                <a href={application.job_url} target="_blank" rel="noopener noreferrer">{application.job_url}</a>
-                                <p>{application.dateApplied}</p>
-                                <p>{application.status}</p>
-                                <p>{application.notes}</p>
-                                <p>{application.updatedAt}</p>
+                            <li key={application.id} className="p-4 bg-gray-200 rounded-md border border-gray-300 flex justify-between gap-4">
+                                <div className="flex flex-col gap-1 flex-1 justify-between"> 
+                                    <h1 className="text-2xl font-bold">{application.company}</h1>
+                                    <h2 className="text-lg font-bold">{application.role}</h2>
+                                    <a href={application.job_url} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 hover:underline">{application.job_url}</a>
+                                    <p className="">
+                                        <span className="font-bold pr-2">Applied on:</span>
+                                    {
+                                        new Date(application.dateApplied).toLocaleDateString(
+                                            'en-US', {
+                                                year: 'numeric',
+                                                month: 'short',
+                                                day: 'numeric'
+                                            }
+                                        )
+                                    }</p>
+                                    <p className="text-sm">Status: <span className="font-bold">{application.status}</span></p>
+                                    <p>
+                                        <span className="font-bold pr-2">Last updated:</span>
+                                        {new Date(application.updatedAt).toLocaleDateString(
+                                            'en-US', {
+                                                year: 'numeric',
+                                                month: 'short',
+                                                day: 'numeric'
+                                            }
+                                        )}
+                                    </p>
+                                </div>
+                                <div className="flex flex-col justify-end items-end gap-2">
+                                    <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                                    <div className='bg-gray-100 rounded-md p-2 w-[200px] min-h-[200px]'>
+                                        <p className="text-sm font-bold mb-1">Notes:</p>
+                                        <p className="text-sm">{application.notes || 'No notes'}</p>
+                                    </div>
+                                </div>
                             </li>
                         ))}
                     </ul>
